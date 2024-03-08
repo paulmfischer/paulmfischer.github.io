e93c9f5a4183e6561600a53f6d3a591f3c6f4e87
Paul Fischer <me@paulmfischer.dev>
2024-03-08 13:54:16 -0600

    handle close sidebar on click out

--- _includes/layouts/main.tsx
+++ _includes/layouts/main.tsx
@@ -11,7 +11,7 @@ export default ({ title, children, comp }: Lume.Data, helpers: Lume.Helpers) => 
     <body className="text-5xl lg:text-base dark:text-slate-300 bg-white dark:bg-zinc-800">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
         <div className="min-h-screen flex">
-          <div id="sidebar" className="max-w-lg hidden lg:flex flex-col fixed inset-0 h-full lg:max-w-64 dark:bg-zinc-800">
+          <div id="sidebar" className="max-w-lg hidden lg:flex flex-col fixed inset-0 h-full lg:max-w-64 dark:bg-zinc-800 z-50">
             <div className="ml-12">
               <button id="close-sidebar" className="mt-5 mr-7 w-9 h-9 lg:w-5 lg:h-5 float-right lg:hidden">
                 <FontAwesomeIcon icon={faXmark} />
@@ -21,7 +21,7 @@ export default ({ title, children, comp }: Lume.Data, helpers: Lume.Helpers) => 
             </div>
             <comp.sidebarFooter></comp.sidebarFooter>
           </div>
-          <div className="max-w-full grow flex flex-col lg:pl-56 dark:bg-zinc-900">
+          <div id="main" className="max-w-full grow flex flex-col lg:pl-56 dark:bg-zinc-900">
             <comp.Header></comp.Header>
             <div id="content" className="ml-10 m-5 mr-12">
               {children}
--- scripts/toggle-sidebar.js
+++ scripts/toggle-sidebar.js
@@ -1,17 +1,23 @@
-console.log('working!');
 const toggleSidebar = () => {
   document.querySelector('#sidebar').classList.toggle('hidden');
   document.querySelector('#sidebar').classList.toggle('flex');
+  const contentEl = document.querySelector('#main');
+  contentEl.removeEventListener('click', toggleSidebar);
 };
+
 
 (function() {
   const displayButton = document.querySelector('#display-navigation');
   if (displayButton) {
-    displayButton.addEventListener('click', toggleSidebar);
+    displayButton.addEventListener('click', (event) => {
+      event.stopPropagation();
+      toggleSidebar();
+      const contentEl = document.querySelector('#main');
+      contentEl.addEventListener('click', toggleSidebar, { once: true })
+    });
   }
   const closeSidebarButton = document.querySelector('#close-sidebar');
   if (closeSidebarButton) {
     closeSidebarButton.addEventListener('click', toggleSidebar);
   }
-  console.log('buttons found!', displayButton, closeSidebarButton);
 })();
